c207b6b177668d9ce99fc58079ca98f6fd738a11
Hadil Guerfi <hdlguerfi@gmail.com>
2022-09-17 14:04:26 +0100

    new changes

--- main.js
+++ main.js
@@ -1,8 +1,41 @@
 
-let list =document.querySelector("header .container i");
 
-let bar=document.querySelector(".main-page-small");
+    let list =document.querySelector("header .container i");
 
-list.onclick=function(){
-bar.classList.toggle("displayed");
+    let bar=document.querySelector(".main-page-small");
+
+    let media =window.matchMedia("(max-width: 768px)");
+
+    bar.classList.add("invisible");
+
+    console.log("hello");
+
+
+
+function myFunction(){
+
+
+if(media.matches){ 
+
+    // bar.classList.remove("invisible");
+
+    list.onclick=function(){
+    
+    bar.classList.toggle("displayed");
+    
+    }
+    console.log("if");
+
 }
+
+else{
+    bar.classList.remove("displayed");
+    bar.classList.add("invisible");
+    console.log("else");
+
+}
+}
+
+
+myFunction(media) ;// Call listener function at run time
+media.addListener(myFunction);
